f82885c84cb4d0e889318d3d74d27d6d139b5efc
José Mario López Leiva <marioleiva2011@gmail.com>
2019-03-12 15:30:38 -0600

    Pagina de registro

--- htdocs/checkRegistro.js
+++ htdocs/checkRegistro.js
@@ -7,6 +7,10 @@ $(document).ready(function ()
 			{
                 required: true
             },
+            nit: 
+			{
+                required: true
+            },
 			usuario: 
 			{
                 required: true
@@ -33,6 +37,7 @@ $(document).ready(function ()
 		messages: 
 		{
 		  nombre: "Debe ingresar su nombre completo",
+		  nit: "Debe ingresar su NIT completo y correcto",
 		  usuario: "Debe ingresar  un nombre de usuario",
 		  correo: "Debe ingresar su correo electrónico",
 		  password: "Ingrese un password de al menos 7 caracteres de longitud",
@@ -45,61 +50,74 @@ $(document).ready(function ()
 			 form.submit();
         }
     });
-	$('#nit4').change(function() {
+	$('#nit').change(function() {
 		//console.log("cambio en usuario");
 
-		var nit1=$( "#nit1").val();
-		var nit2=$( "#nit2").val();
-		var nit3=$( "#nit3").val();
-		var nit4=$( "#nit4").val();
-		var nomUsuario=nit1.concat("-",nit2,"-",nit3,"-",nit4);
-		console.log("el nit completo es; "+nomUsuario);
+		var nit=$("#nit").val();
+
+		//console.log("el nit completo es; "+nomUsuario);
 		//1: comprobar si el nit está escrito correctamente:
 		var re = new RegExp("^([0-9]{4}-[0-9]{6}-[0-9]{3}-[0-9]{1})$");
-		if (re.test(nomUsuario)) {
-		    console.log("Valid");
-		} else {
-		    console.log("Invalid");
+		var estabien=0; //0 es false, 1 es ok que esta en formato NIT
+		if (re.test(nit)) 
+		{
+		    estabien=1;
 		}
 
+		    if(estabien==0)
+			{
+				document.getElementById('nit').style.borderColor = "red";
+				alert("NIT escrito incorrectamente!");
+				document.getElementById('nit').value = "";
+				//si esta mal escrito quito lo del NIT no existe n la base
+				var x = document.getElementById("chequecito");
+				$(x).hide('fast');
+			} 
+
+			else
+			{
+						$.ajax({
+					url:"../comprobarExisteNombre.php?usuario="+nit,
+					success:function(result)
+					{
+						   var codificar=JSON.stringify(result);
+						   document.getElementById('nit').style.borderColor = "green";
+						   if(codificar.localeCompare("false")==0 ) //no existe en bd, ok
+						   {
+								//console.log("Usuario no existe en la bd y está libre");
+								//document.getElementById("divNomUsuario").classList.remove("has-feedback");
+								//document.getElementById("divNomUsuario").classList.add("has-success");
+								$("#divNomUsuario").removeClass("has-error");
+								$("#divNomUsuario").addClass("has-success");
+								var x = document.getElementById("chequecito");
+								if (x.style.display === "none") 
+								{
+									$(x).show('fast');
+								} 	
+								var malo=document.getElementById("chequecitoMalo");
+								malo.style.display = "none";
+						   }
+						   else //existe en la bd, alerto del error
+						   {
+							   var bueno=document.getElementById("chequecito");
+							   bueno.style.display = "none";
+							   $("#divNomUsuario").removeClass("has-success");
+							   	$("#divNomUsuario").addClass("has-error");
+							   	document.getElementById('nit').style.borderColor = "red";
+								//$("#chequecitoMalo").show();
+								var x = document.getElementById("chequecitoMalo");
+								if (x.style.display === "none") 
+								{
+									$(x).show('fast');
+								}
+								
+						   }
+					}
+				}); //fin del ajax
+			} //fin del else si NIT esta bien escrito
 
 		//2: ver si ya está registrado
-		$.ajax({
-			url:"../comprobarExisteNombre.php?usuario="+nomUsuario,
-			success:function(result)
-			{
-				   var codificar=JSON.stringify(result);
-				   if(codificar.localeCompare("false")==0) //no existe en bd, ok
-				   {
-						//console.log("Usuario no existe en la bd y está libre");
-						//document.getElementById("divNomUsuario").classList.remove("has-feedback");
-						//document.getElementById("divNomUsuario").classList.add("has-success");
-						$("#divNomUsuario").removeClass("has-error");
-						$("#divNomUsuario").addClass("has-success");
-						var x = document.getElementById("chequecito");
-						if (x.style.display === "none") 
-						{
-							$(x).show('fast');
-						} 	
-						var malo=document.getElementById("chequecitoMalo");
-						malo.style.display = "none";
-				   }
-				   else //existe en la bd, alerto del error
-				   {
-					   var bueno=document.getElementById("chequecito");
-					   bueno.style.display = "none";
-					   $("#divNomUsuario").removeClass("has-success");
-					   	$("#divNomUsuario").addClass("has-error");
-						//$("#chequecitoMalo").show();
-						var x = document.getElementById("chequecitoMalo");
-						if (x.style.display === "none") 
-						{
-							$(x).show('fast');
-						}
-						
-				   }
-			}
-		}); //fin del ajax
+		
 	});
 	
 	var validator = $("#formRegistro").validate();
